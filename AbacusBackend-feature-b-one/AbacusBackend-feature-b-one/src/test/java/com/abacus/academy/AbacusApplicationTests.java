e686b0e498108674a6388dc5fa0446c5221db957
jerry paramesh <111497116+p-aramesh@users.noreply.github.com>
2022-10-02 21:33:10 +0530

    first commit

---
+++ AbacusBackend-feature-b-one/AbacusBackend-feature-b-one/src/test/java/com/abacus/academy/AbacusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abacus.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
